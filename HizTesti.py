2a9c2f7a1f5b75d5ff127b2d896362ad7b747983
Recepcetinkaya <95712866+Recepcetinkaya@users.noreply.github.com>
2024-12-17 03:07:57 +0300

    Araçların takip edileceği bölge çizildi ve sadece bu bölgedeki araçlar sayıldı

--- HizTesti.py
+++ HizTesti.py
@@ -1,15 +1,19 @@
-## tracking (araç takibi) için byte_track (supervision) izleyiciyi aktifleştir, ve ekranda göster.
-## byte_track = sv.ByteTrack(frame_rate=video_info.fps)
-## detections = byte_track.update_with_detections(detections=detections)
+## poligon zon bölgesi tanımlama ve sadece o bölgedeki araçları algılatıp gösterme
+# SOURCE = np.array([[int(1252*0.3), int(787*0.3)], [int(2298*0.3), int(803*0.3)],
+#                    [int(5039*0.3), int(2159*0.3)], [int(-550*0.3), int(2159*0.3)]])
 
-# labels = []
-#         for tracker_id in np.array(detections.tracker_id):
-#             labels.append(f"#{tracker_id}")
+## polygon_zone = sv.PolygonZone(polygon=SOURCE, frame_resolution_wh=video_info.resolution_wh)
+## detections = detections[polygon_zone.trigger(detections)]
+## annotated_frame = sv.draw_polygon(annotated_frame, polygon=SOURCE, color=sv.Color.red())
+
 
 import supervision as sv
 import cv2
 from ultralytics import YOLO
 import numpy as np
+
+SOURCE = np.array([[int(1252*0.3), int(787*0.3)], [int(2298*0.3), int(803*0.3)],
+                   [int(5039*0.3), int(2159*0.3)], [int(-550*0.3), int(2159*0.3)]])
 
 if __name__ == "__main__":
 
@@ -21,7 +25,8 @@ if __name__ == "__main__":
     thickness = 3
     text_scale = 1
 
-    bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=thickness)
+
+    bounding_box_annotator = sv.BoxAnnotator(thickness=thickness)
     label_annotator = sv.LabelAnnotator(
         text_scale=text_scale,
         text_thickness=thickness,
@@ -29,10 +34,13 @@ if __name__ == "__main__":
 
     frame_generator = sv.get_video_frames_generator(source_path="vehicles.mp4")
 
+    polygon_zone = sv.PolygonZone(polygon=SOURCE)
+
     for frame in frame_generator:
         half_frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
         result = model(half_frame)[0]
         detections = sv.Detections.from_ultralytics(result)
+        detections = detections[polygon_zone.trigger(detections)]
         detections = byte_track.update_with_detections(detections=detections)
 
         labels = []
@@ -40,6 +48,7 @@ if __name__ == "__main__":
             labels.append(f"#{tracker_id}")
 
         annotated_frame = half_frame.copy()
+        annotated_frame = sv.draw_polygon(annotated_frame, polygon=SOURCE, color=sv.Color.RED)
         annotated_frame = bounding_box_annotator.annotate(
             scene=annotated_frame, detections=detections)
 
