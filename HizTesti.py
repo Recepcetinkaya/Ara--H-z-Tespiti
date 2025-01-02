1962c12e4e3fcad9da8bb208437d2881252affa8
Recepcetinkaya <95712866+Recepcetinkaya@users.noreply.github.com>
2025-01-02 14:06:36 +0300

    Araçlar kategorilerine göre renklendirildi, numaralandırıldı ve anlık hızları tespit edilip üzerinde gösterildi.Proje son commit'idir

--- HizTesti.py
+++ HizTesti.py
@@ -1,18 +1,21 @@
-## perspektif görünümü opencv getPerspectiveTransform ile doğrultulmuş düz görünümlü matrise dönüştürme
-## bu dönüştürülmüş matrisi kullanarak araçların yaklaşık x,y koordinatlarını hesaplama
 
-## view_transformer = ViewTransformer(source=SOURCE, target=TARGET)
-# points = detections.get_anchors_coordinates(anchor=sv.Position.BOTTOM_CENTER)
-# points = view_transformer.transform_points(points=points).astype(int)
+# araçların arkasından iz çizgisi çizdirme (trace_annotator ile)
+# boş points değerleri için hata giderme
 
-# labels = [f"x: {x}, y: {y}"
-#             for [x,y] in points]
+# trace_annotator = sv.TraceAnnotator(
+#         thickness=thickness,
+#         trace_length=video_info.fps * 2,
+#         position=sv.Position.BOTTOM_CENTER)
 
+# annotated_frame = trace_annotator.annotate(scene=annotated_frame, detections=detections)
+
+#  hata giderildi:  if len(points) != 0:
 
 import supervision as sv
 import cv2
 from ultralytics import YOLO
 import numpy as np
+from collections import defaultdict, deque
 
 SOURCE = np.array([[int(1252*0.3), int(787*0.3)], [int(2298*0.3), int(803*0.3)],
                    [int(5039*0.3), int(2159*0.3)], [int(-550*0.3), int(2159*0.3)]])
@@ -36,9 +39,17 @@ class ViewTransformer:
         self.m = cv2.getPerspectiveTransform(source, target)
 
     def transform_points(self, points: np.ndarray) -> np.ndarray:
-        reshaped_points = points.reshape(-1, 1, 2).astype(np.float32)
-        transformed_points = cv2.perspectiveTransform(reshaped_points, self.m)
-        return transformed_points.reshape(-1, 2)
+        print("points :", points)
+        if len(points) != 0:
+
+            reshaped_points = points.reshape(-1, 1, 2).astype(np.float32)
+            transformed_points = cv2.perspectiveTransform(reshaped_points, self.m)
+
+            return transformed_points.reshape(-1, 2)
+
+        else:
+            hata_giderici_gecici_deger = np.array([[1, 2], [5, 8]], dtype='float')
+            return hata_giderici_gecici_deger
 
 
 if __name__ == "__main__":
@@ -55,13 +66,20 @@ if __name__ == "__main__":
     label_annotator = sv.LabelAnnotator(
         text_scale=text_scale,
         text_thickness=thickness,
-        text_position=sv.Position.BOTTOM_CENTER)
+        text_position=sv.Position.BOTTOM_CENTER, color_lookup=sv.ColorLookup.TRACK)
+
+    trace_annotator = sv.TraceAnnotator(
+        thickness=thickness,
+        trace_length=video_info.fps * 2,
+        position=sv.Position.BOTTOM_CENTER, color_lookup=sv.ColorLookup.TRACK)
 
     frame_generator = sv.get_video_frames_generator(source_path="vehicles.mp4")
 
     polygon_zone = sv.PolygonZone(polygon=SOURCE)
 
     view_transformer = ViewTransformer(source=SOURCE, target=TARGET)
+
+    coordinates = defaultdict(lambda: deque(maxlen=video_info.fps))
 
     for frame in frame_generator:
         half_frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
@@ -73,15 +91,24 @@ if __name__ == "__main__":
         points = detections.get_anchors_coordinates(anchor=sv.Position.BOTTOM_CENTER)
         points = view_transformer.transform_points(points=points).astype(int)
 
-        labels = [
-            f"x: {x}, y: {y}"
-            for [x,y] in points]
+        for tracker_id, [_, y] in zip(detections.tracker_id, points):
+            coordinates[tracker_id].append(y)
 
-        # for tracker_id in np.array(detections.tracker_id):
-        #     labels.append(f"#{tracker_id}")
+        labels = []
+        for tracker_id in detections.tracker_id:
+            if len(coordinates[tracker_id]) < video_info.fps / 2:
+                labels.append(f"#{tracker_id}")
+            else:
+                coordinate_start = coordinates[tracker_id][-1]
+                coordinate_end = coordinates[tracker_id][0]
+                distance = abs(coordinate_start - coordinate_end)
+                time = len(coordinates[tracker_id]) / video_info.fps
+                speed = distance / time * 3.6
+                labels.append(f"#{tracker_id} {int(speed)} km/h")
 
         annotated_frame = half_frame.copy()
         annotated_frame = sv.draw_polygon(annotated_frame, polygon=SOURCE, color=sv.Color.RED)
+        annotated_frame = trace_annotator.annotate(scene=annotated_frame, detections=detections)
         annotated_frame = bounding_box_annotator.annotate(
             scene=annotated_frame, detections=detections)
 
